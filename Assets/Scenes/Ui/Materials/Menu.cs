8aa637113dbac6901a75aacbdf052467a0b7359d
Hakiio2004 <Sadtawutsommai2004@gmail.com>
2025-03-29 15:30:56 +0700

    กดเริ่มเกมได้เเล้ว

---
+++ Assets/Scenes/Ui/Materials/Menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // ฟังก์ชันสำหรับปุ่ม Start
+    public void StartGame()
+    {
+        Debug.Log("Start button clicked!");
+        SceneManager.LoadScene("Game"); 
+    }
+
+    // ฟังก์ชันสำหรับปุ่ม Quit
+    public void QuitGame()
+    {
+        Debug.Log("Game is quitting...");
+        Application.Quit();
+    }
+}
